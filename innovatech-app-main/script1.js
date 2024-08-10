3e65d6adf765a0efbf82fde40d38873337d3ff66
nidhi <dnai@gmail.com>
2024-08-10 16:37:26 +0530

    soil js

--- innovatech-app-main/script1.js
+++ innovatech-app-main/script1.js
@@ -1,18 +1,23 @@
 document.querySelector(".submit").addEventListener("click", (event) => {
   event.preventDefault();
   setTimeout(() => {
+    let nitrogen_data = document.querySelector(".nitrogen").value;
+    let phosphorus_data = document.querySelector(".phosphorus").value;
+    let potassium_data = document.querySelector(".potassium").value;
+    let humidity_data = document.querySelector(".humidity").value;
+    let temperature_data = document.querySelector(".temperature").value;
+    let rainfall_data = document.querySelector(".rainfall").value;
+    localStorage.setItem("nitrogen_data", nitrogen_data);
+    localStorage.setItem("phosphorus_data", phosphorus_data);
+    localStorage.setItem("potassium_data", potassium_data);
+    localStorage.setItem("humidity_data", humidity_data);
+    localStorage.setItem("temperature_data", temperature_data);
+    localStorage.setItem("rainfall_data", rainfall_data);
     document.querySelector(".page1_form").style.display = "none";
+    
+    const valueSpans = document.querySelectorAll('.soil_data span[id$="-value"]');
+    let soil_data=document.querySelector(".soil_data");
+    soil_data.style.display = "block";
+    // soil_data.
   }, 500);
-  let nitrogen_data = document.querySelector(".nitrogen").value;
-  let phosphorus_data = document.querySelector(".phosphorus").value;
-  let potassium_data = document.querySelector(".potassium").value;
-  let humidity_data = document.querySelector(".humidity").value;
-  let temperature_data = document.querySelector(".temperature").value;
-  let rainfall_data = document.querySelector(".rainfall").value;
-  localStorage.setItem("nitrogen_data", nitrogen_data);
-  localStorage.setItem("phosphorus_data", phosphorus_data);
-  localStorage.setItem("potassium_data", potassium_data);
-  localStorage.setItem("humidity_data", humidity_data);
-  localStorage.setItem("temperature_data", temperature_data);
-  localStorage.setItem("rainfall_data", rainfall_data);
 });
